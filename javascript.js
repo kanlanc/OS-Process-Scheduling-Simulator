3149ee8e9195411c34db189906b57e877740c961
saivicky2015 <saivicky2015@gmail.com>
2017-09-16 01:31:09 +0530

    After shj

--- javascript.js
+++ javascript.js
@@ -10,48 +10,73 @@ function algorithm() {
     var number = document.getElementById('number').value;
     var ans = document.getElementById('ans');
     var process1_name = document.getElementById('first_process').value;
-    var process1_value = document.getElementById('first_process_com').value;
+    var process1_value = document.getElementById('first_process_com').value*1;
 
     var process2_name = document.getElementById("second_process").value;
-    var process2_value = document.getElementById("second_process_com").value;
+    var process2_value = document.getElementById("second_process_com").value*1;
 
     var process3_name = document.getElementById("third_process").value;
-    var process3_value = document.getElementById("third_process_com").value;
+    var process3_value = document.getElementById("third_process_com").value*1;
 
     var process4_name = document.getElementById("fourth_process").value;
-    var process4_value = document.getElementById("fourth_process_com").value;
+    var process4_value = document.getElementById("fourth_process_com").value*1;
 
     var process5_name = document.getElementById("fifth_process").value;
-    var process5_value = document.getElementById("fifth_process_com").value;
+    var process5_value = document.getElementById("fifth_process_com").value*1;
 
     var process6_name = document.getElementById("sixth_process").value;
-    var process6_value = document.getElementById("sixth_process_com").value;
+    var process6_value = document.getElementById("sixth_process_com").value*1;
 
     var process7_name = document.getElementById("seventh_process").value;
-    var process7_value = document.getElementById("seventh_process_com").value;
+    var process7_value = document.getElementById("seventh_process_com").value*1;
 
     var process8_name = document.getElementById("eighth_process").value;
-    var process8_value = document.getElementById("eighth_process_com").value;
+    var process8_value = document.getElementById("eighth_process_com").value*1;
 
     var process9_name = document.getElementById("ninth_process").value;
-    var process9_value = document.getElementById("ninth_process_com").value;
+    var process9_value = document.getElementById("ninth_process_com").value*1;
+    var process_order="start";
+    var array=[process1_value,process2_value,process3_value,process4_value,process5_value,process6_value,process7_value,process8_value,process9_value];
+    var dict={};
+    dict[process1_value]=process1_name;
+    dict[process2_value]=process2_name;
+    dict[process3_value]=process3_name;
+    dict[process4_value]=process4_name;
+    dict[process5_value]=process5_name;
+    dict[process6_value]=process6_name;
+    dict[process7_value]=process7_name;
+    dict[process8_value]=process8_name;
+    dict[process9_value]=process9_name;
     if (value == 0) {
             //do nothing                     
     }
     else if (value == 1) {
             //fcfs
-            var average_time_num = process1_value*1 + process2_value*1 + process3_value*1 + process4_value*1 + process5_value*1 + process6_value*1 + process7_value*1 + process8_value*1 + process9_value*1;
+
+            var average_time_num = process1_value + process2_value + process3_value + process4_value + process5_value + process6_value + process7_value + process8_value+ process9_value;
             var average_time = average_time_num / number;
-
-            console.log(average_time_num);
+            for(var i=0;i<array.length;i++)
+            {
+                var xam = dict[array[i].toString()];
+                process_order=process_order+","+xam;
+            }
             ans.innerHTML = average_time;
-
-
-
+            ans1.innerHTML = process_order;
     }
 
     else if (value == 2) {
             //shj
+            var average_time_num = process1_value + process2_value + process3_value + process4_value + process5_value + process6_value + process7_value + process8_value+ process9_value;
+            var average_time = average_time_num / number;
+            array.sort();
+            for(var i=0;i<array.length;i++)
+            {
+                var xam = dict[array[i].toString()];
+                process_order=process_order+","+xam;
+            }
+            ans.innerHTML = average_time;
+            ans1.innerHTML = process_order;
+
 
     }
     else if (value == 3) {
